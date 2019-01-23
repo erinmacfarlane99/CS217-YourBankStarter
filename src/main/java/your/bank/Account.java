796de007644da85aef50705a5e7d0e02d959a54a
Ewan Skene <ewan.skene.2017@uni.strath.ac.uk>
2019-01-23 22:21:45 +0000

    created new html page and worked on displaying account details

--- src/main/java/your/bank/Account.java
+++ src/main/java/your/bank/Account.java
@@ -4,17 +4,38 @@ import java.math.BigDecimal;
 
 import static java.math.BigDecimal.*;
 
+import java.text.DecimalFormat;
+
 public class Account {
 
     private BigDecimal balance;
-
+    private String accName;
 
     public Account() {
+        accName = "placeholder_name";
         balance = new BigDecimal(0);
     }
 
     public Account(double amount) {
+        accName = "placeholder_name";
         balance = new BigDecimal(amount);
+    }
+
+    public Account(String name, double amount) {
+        balance = new BigDecimal(amount);
+        accName = name;
+    }
+
+    public String getAccountName() {
+        return accName;
+    }
+
+    public void setAccountName(String accName) {
+        this.accName = accName;
+    }
+
+    public double getBalance() {
+        return balance.doubleValue();
     }
 
     public void deposit(double amount) {
@@ -30,7 +51,10 @@ public class Account {
 
     }
 
-    public double getBalance() {
-        return balance.doubleValue();
+    @Override
+    public String toString() {
+        return "name: " + this.getAccountName() + " balance: " + new DecimalFormat("#.00").format(this.getBalance());
     }
+
+
 }
--- src/main/java/your/bank/App.java
+++ src/main/java/your/bank/App.java
@@ -10,6 +10,8 @@ import org.jooby.jdbc.Jdbc;
 import org.jooby.json.Jackson;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -17,60 +19,78 @@ import java.util.Random;
  */
 public class App extends Jooby {
 
-  {
-    // -- Start Boilerplate Setup --
-    use(new UnirestHelper());
-    use(new Hbs());
-    use(new Jackson());
-    use(new Jdbc("db"));
+    private List<Account> accountList = new ArrayList<>();
 
-    assets("/bootstrap/**");
-    assets("/static/**");
+    {
+        // -- Start Boilerplate Setup --
+        use(new UnirestHelper());
+        use(new Hbs());
+        use(new Jackson());
+        use(new Jdbc("db"));
 
-    // -- End Boilerplate Setup --
+        assets("/bootstrap/**");
+        assets("/static/**");
 
-    // Simple GET Request
-    get("/", () -> "Hello World!");
+        // -- End Boilerplate Setup --
+
+        // Simple GET Request
+        get("/", () -> "Hello World!");
 
 
-    // GET request which makes a call to another endpoint
-    get("/hello",() -> {
-      return "Hello " + Unirest.get("http://faker.hook.io/").asString().getBody();
-    });
+        // GET request which makes a call to another endpoint
+        get("/hello", () -> {
+            return "Hello " + Unirest.get("http://faker.hook.io/").asString().getBody();
+        });
 
-    // GET request with a template and a query parameter /dice?name=Jim
-    get("/dice", (request)->{
-      String name = "Your";
+        // GET request with a template and a query parameter /dice?name=Jim
+        get("/dice", (request) -> {
+            String name = "Your";
 
-      // Mutant is Jooby's representation of data which has not been cast to a type (and could be null/not set)
-      Mutant query = request.param("name");
-      if(query.isSet()) {
-        name = query.value() + "'s";
-      }
+            // Mutant is Jooby's representation of data which has not been cast to a type (and could be null/not set)
+            Mutant query = request.param("name");
+            if (query.isSet()) {
+                name = query.value() + "'s";
+            }
 
-      // This renders public/dice.html with the model populated with random and name
-      return Results.html("dice")
-              .put("random",new Random().nextInt(6))
-              .put("name",name);
-    });
+            // This renders public/dice.html with the model populated with random and name
+            return Results.html("dice")
+                    .put("random", new Random().nextInt(6))
+                    .put("name", name);
+        });
 
-    // Perform actions on startup
-    onStart(() -> {
-      System.out.println("Starting Up...");
+        get("/details", () -> {
+            String out = new String();
+            for (Account a : accountList) {
+                out += a.toString() + " <br> ";
+            }
+            return Results.html("Accounts")
+                    .put("details", out);
+        });
 
-        DataSource ds = require(DataSource.class);
+        // Perform actions on startup
+        onStart(() -> {
+            System.out.println("Starting Up...");
 
-    });
+            DataSource ds = require(DataSource.class);
 
-    // Perform actions after startup
-    onStarted(() -> {
-      System.out.println("Started!");
-    });
+            accountList.add(new Account("Rachel", 50.00));
+            accountList.add(new Account("Monica", 100.00));
+            accountList.add(new Account("Phoebe", 76.00));
+            accountList.add(new Account("Joey", 23.90));
+            accountList.add(new Account("Chandler", 3.00));
+            accountList.add(new Account("Ross", 54.32));
 
-  }
+        });
 
-  public static void main(final String[] args) {
-    run(App::new, args);
-  }
+        // Perform actions after startup
+        onStarted(() -> {
+            System.out.println("Started!");
+        });
+
+    }
+
+    public static void main(final String[] args) {
+        run(App::new, args);
+    }
 
 }

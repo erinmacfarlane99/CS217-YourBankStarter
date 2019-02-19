5a0788dadab9639cc7cccac8fb5344f768a341e5
Ewan Skene <ewancskene@gmail.com>
2019-02-19 17:24:44 +0000

    add transaction data class

---
+++ src/main/java/your/bank/Transaction.java
@@ -0,0 +1,48 @@
+package your.bank;
+
+public class Transaction {
+    private String id;
+    private double amount;
+    private String from;
+    private String to;
+
+    public Transaction(String id, double amount, String from, String to) {
+        this.id = id;
+        this.amount = amount;
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+}

ded7fdae652654523ad620c26e244825395d677b
Ewan Skene <ewancskene@gmail.com>
2019-02-20 21:35:57 +0000

    add bankdata test class

---
+++ src/test/java/your/bank/BankDataTests.java
@@ -0,0 +1,11 @@
+package your.bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+public class BankDataTests {
+
+    //TODO
+
+}
